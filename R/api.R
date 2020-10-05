ebfeffba60286ac3e8a8fe347ed1b6e97b4be84e
DenUsenko <usenko.pro@gmail.com>
2020-10-05 17:34:10 +0000

    Write add_auth_params function
    Adds credentials to the API request

--- R/api.R
+++ R/api.R
@@ -10,3 +10,21 @@ build_endpoint_url <- function(endpoint) {
   endpoint_url <- paste0(api_base_url, api_version, "/", endpoint, ".json")
   return(endpoint_url)
 }
+
+
+#' Add credentials to the API request
+#'
+#' Add key and secret to the list of parameters for request
+#'
+#' @param params named list of parameters
+#' @param api_key client API key
+#' @param api_secret client API secret
+add_auth_params <- function(params, api_key = NULL, api_secret = NULL) {
+  if (is.null(api_key)) api_key <- getOption("rbinotel.api_key")
+  if (is.null(api_secret)) api_secret <- getOption("rbinotel.api_secret")
+  signed_params <- append(params, list(key = api_key, secret = api_secret))
+  return(signed_params)
+}
+
+# TODO: get key / secret and validate them
+# TODO: check API error code and return message
