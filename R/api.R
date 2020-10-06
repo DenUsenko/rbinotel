03466534a76aa7c6e28e023eb126cbe236b37647
DenUsenko <usenko.pro@gmail.com>
2020-10-06 19:17:32 +0000

    Add send_api_request
    Added main function to send POST requests

--- R/api.R
+++ R/api.R
@@ -2,7 +2,7 @@
 #'
 #' Take an endpoint and attach it to the base url
 #'
-#' @param endpoint an API endpoint without leading and trailing slashes"
+#' @param endpoint an API endpoint without leading and trailing slashes
 build_endpoint_url <- function(endpoint) {
   assertive::assert_is_character(endpoint)
   api_version <- getOption("rbinotel.api_version")
@@ -10,7 +10,6 @@ build_endpoint_url <- function(endpoint) {
   endpoint_url <- paste0(api_base_url, api_version, "/", endpoint, ".json")
   return(endpoint_url)
 }
-
 
 #' Add credentials to the API request
 #'
@@ -26,5 +25,51 @@ add_auth_params <- function(params, api_key = NULL, api_secret = NULL) {
   return(signed_params)
 }
 
+#' Send request to Binotel API
+#'
+#' Send POST request with parameters and key/secret to /code{endpoint}
+#'
+#' @param endpoint API endpoint specific to the request
+#' @param params list of parameters for request
+send_api_request <- function(endpoint, params) {
+  url <- build_endpoint_url(endpoint)
+  params <- add_auth_params(params)
+  request_body <- jsonlite::toJSON(params)
+  resp <- httr::POST(url, body = request_body, encode = "raw")
+
+  # Check for errors in response
+
+  if (httr::http_type(resp) != "application/json") {
+    stop("Binotel API did not return json", call. = FALSE)
+  }
+
+  if (httr::status_code(resp) != 200) {
+    stop(
+      sprintf(
+        "Binotel API request failed  [%s]",
+        httr::status_code(resp)
+      ),
+      call. = FALSE
+    )
+  }
+
+  parsed <- jsonlite::fromJSON(httr::content(resp, as = "text", encoding = "UTF-8"), simplifyVector = FALSE)
+
+  if (parsed$status == "error") {
+    stop(
+      sprintf(
+        "Binotel API request resulted in an error [%d]\n%s",
+        resp$code,
+        resp$message
+      ),
+      call. = FALSE
+    )
+  }
+
+  # Return API response
+  return(parsed)
+
+}
+
 # TODO: get key / secret and validate them
-# TODO: check API error code and return message
+# TODO: use @importFrom
--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -2,3 +2,5 @@ test_that("Full endpoint url is build", {
   expect_equal(build_endpoint_url("test/endpoint"), "https://api.binotel.com/api/4.0/test/endpoint.json")
   expect_error(build_endpoint_url(123), "is not of class 'character'")
 })
+
+# TODO: add test for send_api_request()
